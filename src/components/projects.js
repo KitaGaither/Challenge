3ae1381ab814d92e3f08520b309b52b3715dc679
Lakita Gaither <lakitagaither@yahoo.com>
2022-01-18 18:04:39 -0600

    App Structure

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,7 +3,9 @@ import React from "react";
 function Projects() {
     return (
         <React.Fragment>
-            <div className='projectspage-div'></div>
+            <div className='projectspage-div'>
+                Yall these are my projects!
+            </div>
             <projects />
         </React.Fragment>
         
